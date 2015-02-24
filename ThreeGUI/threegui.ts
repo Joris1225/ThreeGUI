673ed553d9aa6a4ef934e9b6b14ed8db7c245e71
Joris <jorisvanvugt@live.nl>
2015-02-24 02:04:12 +0100

    Draw text to screen

--- ThreeGUI/threegui.ts
+++ ThreeGUI/threegui.ts
@@ -5,26 +5,35 @@
         private canvas: HTMLCanvasElement;
 
         constructor(gameCanvas: HTMLCanvasElement) {
-            this.canvas = document.createElement("canvas");
+            this.canvas = document.createElement('canvas');
+            this.canvas.style.zIndex = "10";
 
             this.canvas.width = gameCanvas.width;
             this.canvas.height = gameCanvas.height;
             var location: ClientRect = gameCanvas.getBoundingClientRect();
-            this.canvas.style.position = "absolute";
-            this.canvas.style.top = location.top.toString() + "px";
-            this.canvas.style.left = location.left.toString() + "px";
-            this.ctx = this.canvas.getContext("2d");
+            this.canvas.style.position = 'absolute';
+            this.canvas.style.top = location.top.toString() + 'px';
+            this.canvas.style.left = location.left.toString() + 'px';
+            this.ctx = this.canvas.getContext('2d');
             if (this.ctx == null)
-                console.error("Could not create 2D context. Consider upgrading your browser!");
+                console.error('Could not create 2D context. Consider upgrading your browser!');
             gameCanvas.parentNode.appendChild(this.canvas);
-            this.render();
         }
 
-        private render() {
-            this.ctx.fillStyle = "#00ff00";
+        public clear(): void {
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-            this.ctx.fillText("test1234", 10, 10);
+        }
 
+        public setFillColor(fillStyle : any): void {
+            this.ctx.fillStyle = fillStyle; 
+        }
+
+        public fillText(text: string, x: number, y: number): void {
+            this.ctx.fillText(text, x, y);
+        }
+
+        public setFont(font: string): void {
+            this.ctx.font = font;
         }
 
     }
